c906181b48518f5827e0008f8996ebac5941bd75
jean <jean1xd@gmail.com>
2024-11-20 12:46:48 -0500

    reactjs cards fetchdata, conection supabase, dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,75 +1,162 @@
 import { sql } from '@vercel/postgres';
+
+import { createClient } from '@supabase/supabase-js';
 import {
   CustomerField,
   CustomersTableType,
   InvoiceForm,
   InvoicesTable,
+  LatestInvoice,
   LatestInvoiceRaw,
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { log } from 'console';
 
+// Lee las variables de entorno de tu archivo .env.local
+// const SUPABASE_URL = process.env.SUPABASE_URL;
+// const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+const supabase = createClient('https://dzblbuhwqjigieabdomi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YmxidWh3cWppZ2llYWJkb21pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwNDQxMjQsImV4cCI6MjA0NzYyMDEyNH0.4bV7yYtkM5hAvSc9TNklvS4zlyPrVTegmwAcBGj3d0g')
+// const supabase1 = createClient<Database>( process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+
+//DEFINIMOS UN TIPO PARA QUE TS DETECTE LOS RESULTADOS DEVUELTOS POR SUPA
+type InvoiceStatus = {
+  paid: number | null; // o string, dependiendo de cómo manejes los montos
+  pending: number | null; // o string
+};
+//SQL SUPABASE
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+    // const { data } = await supabase.from('revenue').select()
+    const { data } = await supabase.from('revenue').select('*')
+    return data || [];  // Aseguramos que nunca sea null, siempre será un arreglo.
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error('Supabase Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
 }
-
+// const { data } = await supabase
+// .from('customers')
+// .select(`
+//   id, 
+//   name, 
+//   image_url, 
+//   email, 
+//   invoices (id, amount, status, date)
+// `)
+// // .order('date', {ascending: false})
+// .limit(5)
+//##supabase
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
-
-    const latestInvoices = data.rows.map((invoice) => ({
-      ...invoice,
+    const { data } = await supabase
+    .from('invoices')
+    .select(`
+      id, amount,
+      customers (name, image_url, email)
+    `)
+    .order('date', {ascending: false})
+    .limit(5);
+    
+    const latestInvoices = (data || []).map((invoice) => ({
+      ...invoice.customers,
       amount: formatCurrency(invoice.amount),
+      id: invoice.id,
     }));
-    return latestInvoices;
+    //@ts-ignore #TODO: BUG DE TYPESCRIP NO RECONOCE LA DESESTRUCTURACION
+    return latestInvoices as LatestInvoice[] || [];
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch the latest invoices.');
   }
 }
 
+
+//1. EXPLICACION DEL CODIGO
+//2. Obtener el Estado de las Facturas
+// const invoiceStatusPromise = supabase
+//   .from('invoices')
+//   .select('status, amount')
+// Objetivo: Aquí, se crea otra promesa (invoiceStatusPromise) que recupera el estado y el monto de cada factura de la tabla invoices.
+// select('status, amount'): Esto indica que deseas obtener dos columnas: status (el estado de la factura, que puede ser 'paid' o 'pending') y amount (el monto de la factura).
+
+// 3. Manejo de Resultados con Promesas
+// .then(({ data, error }) => {
+//   if (error) throw new Error(error.message);
+// Objetivo: Una vez que se completa la consulta a la tabla invoices, se utiliza el método .then() para manejar el resultado.
+// Desestructuración: Se desestructuran los resultados en data y error. data contendrá los registros obtenidos, mientras que error contendrá información sobre cualquier error que haya ocurrido durante la consulta.
+// Manejo de Errores: Si hay un error, se lanza una nueva excepción con el mensaje de error, lo que detiene la ejecución y permite manejar el error en otra parte del código.
+
+// 4. Calcular Totales
+// const totals = data.reduce((acc, invoice) => {
+//   if (invoice.status === 'paid') {
+//     acc.paid += invoice.amount;
+//   } else if (invoice.status === 'pending') {
+//     acc.pending += invoice.amount;
+//   }
+//   return acc;
+// }, { paid: 0, pending: 0 });
+// Objetivo: Esta sección utiliza reduce para calcular el total de montos de las facturas pagadas y pendientes.
+// data.reduce(...): reduce es un método de array que aplica una función a un acumulador y a cada valor de la matriz (en este caso, cada factura) para reducirlo a un solo valor.
+// (acc, invoice): acc es el acumulador (un objeto que lleva el total de montos) y invoice es el elemento actual (cada factura).
+// Condicionales:
+// Si invoice.status === 'paid': Se suma el monto de la factura al total de paid.
+// Si invoice.status === 'pending': Se suma el monto de la factura al total de pending.
+// Valor Inicial: Se especifica { paid: 0, pending: 0 } como el valor inicial del acumulador acc.
+
+// 5. Devolver Totales
+// return totals; Objetivo: Finalmente, la función then devuelve el objeto totals, que contiene los totales acumulados de las facturas pagadas y pendientes.
+
+
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+    // Realiza las consultas a Supabase en paralelo
+    const invoiceCountPromise = supabase
+      .from('invoices')
+      //es mas rapido que seleccionar todo
+      .select('id', { count: 'exact' });
 
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
+    const customerCountPromise = supabase
+      .from('customers')
+      .select('id', { count: 'exact' });
+
+      const invoiceStatusPromise = supabase
+      .from('invoices')
+      .select('status, amount')
+      .then(({ data, error }) => {
+        if (error) throw new Error(error.message);
+        //reduce recorre todos los elementos y devuelve solo uno (la sumatoria o final)
+        //mientras eso pasa se usa su acumulador y se recorre cada elemento
+        const totals = data.reduce((acc, invoice) => {
+          if (invoice.status === 'paid') {
+            acc.paid += invoice.amount;
+          } else if (invoice.status === 'pending') {
+            acc.pending += invoice.amount;
+          }
+          return acc;//retornamos acc porque es el acumulador(row(fila) final o sumado)
+        }, { paid: 0, pending: 0 });
+    
+        return totals;//retornamos el valor final(que solo es uno)
+      });
+
+
+    const [invoiceCountResult, customerCountResult, invoiceStatusResult] = await Promise.all([
+      invoiceCountPromise,//invoiceCountResult cada uno corresponde a cada uno de los parametros
+      customerCountPromise,//customerCountResult
+      invoiceStatusPromise,//invoiceStatusResult
     ]);
+    
+    // Extrae los datos
+    const numberOfInvoices = Number(invoiceCountResult.count ?? '0');
+    const numberOfCustomers = Number(customerCountResult.count ?? '0');
+    
+    // Asegúrate de que statusData tenga el formato correcto y maneja su valor
+    const totalPaidInvoices = formatCurrency(invoiceStatusResult?.paid ?? 5);
+    const totalPendingInvoices = formatCurrency(invoiceStatusResult?.pending ?? 50)
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
 
     return {
       numberOfCustomers,
@@ -82,7 +169,6 @@ export async function fetchCardData() {
     throw new Error('Failed to fetch card data.');
   }
 }
-
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
   query: string,
