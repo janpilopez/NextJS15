97633b80314d29530cbf73aca8a1550d2f2e1a03
Jean Lopez <jlopez@pespesca.com>
2025-04-03 11:10:44 -0500

    avanze base de datos

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -19,7 +19,7 @@ export default async function Page( props: { params: Promise<{ id: string }> }) 
     ])
 
 
-    
+
     if (!invoice) {
         notFound();
     }
@@ -27,7 +27,7 @@ export default async function Page( props: { params: Promise<{ id: string }> }) 
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    { label: 'Invoices Facturacion', href: '/dashboard/invoices' },
                     {
                         label: 'Edit Invoice',
                         href: `/dashboard/invoices/${id}/edit`,
